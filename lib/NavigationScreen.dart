e7d3ae49250cc3ee3f12adb3b8c58c4a36973a81
oscar1081865 <oscar1081865@gmail.com>
2022-04-26 13:13:57 -0500

    changes

---
+++ lib/NavigationScreen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:senior_project/Login_Page.dart';
+import 'AccountScreen.dart';
+import 'Login_Page.dart';
+import 'SelectCurrencyPage.dart';
+
+
+class NavigationScreen extends StatefulWidget{
+  @override
+  _NavigationScreen createState() => _NavigationScreen();
+}
+
+class _NavigationScreen extends State<NavigationScreen> {
+  int currentIndex = 0;
+  final screens = [
+    AccountScreen(),
+    AccountScreen(),
+    SelectCurrencyPage(),
+    LoginPage(),
+  ];
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: screens[currentIndex],
+      bottomNavigationBar: new BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) =>
+          setState(() =>
+           currentIndex = index),
+        backgroundColor: Colors.green,
+        items: const <BottomNavigationBarItem>[
+           BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.money),
+            label: 'Deposit',
+            backgroundColor: Colors.green
+          ),
+           BottomNavigationBarItem(
+            icon: Icon(Icons.change_circle_outlined),
+            label: 'Exchange',
+            backgroundColor: Colors.green
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.exit_to_app),
+            label: 'Sign Out',
+            backgroundColor: Colors.green
+          ),
+        ],
+      ),
+    );
+  }
+}
